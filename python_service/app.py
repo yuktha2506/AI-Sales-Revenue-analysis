de75f1b207e5e2dea11ef3e0ff180270536c5178
Yuktha Shree P <yukthashree456@gmail.com>
2026-05-19 20:54:35 +0530

    ..

--- python_service/app.py
+++ python_service/app.py
@@ -18,7 +18,9 @@ def engine():
     host = os.getenv("MYSQL_HOST", os.getenv("DB_HOST", "localhost"))
     port = os.getenv("MYSQL_PORT", os.getenv("DB_PORT", "3306"))
     db = os.getenv("MYSQL_DATABASE", os.getenv("DB_NAME", "sales_analytics"))
-    return create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}", pool_pre_ping=True)
+    ca_path = os.getenv("MYSQL_SSL_CA_PATH", os.getenv("DB_SSL_CA_PATH"))
+    connect_args = {"ssl": {"ca": ca_path}} if ca_path else {}
+    return create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}", pool_pre_ping=True, connect_args=connect_args)
 
 DB_ENGINE = engine()
 
