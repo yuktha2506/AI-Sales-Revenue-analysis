067fcbc7fa5a9df0573a80761894acdd21e3c331
Yuktha Shree P <yukthashree456@gmail.com>
2026-05-20 19:01:24 +0530

    ..

--- database/performance_indexes.sql
+++ database/performance_indexes.sql
@@ -1,5 +1,3 @@
-USE sales_analytics;
-
 CREATE INDEX idx_orders_user_date ON orders(user_id, date);
 CREATE INDEX idx_order_items_product ON order_items(product_id);
 CREATE INDEX idx_order_items_order ON order_items(order_id);
